8071c1544e3fc837bf21d3d85d0d0ddf9d1166c4
iagobraatz <iagobraatz.21@gmail.com>
2026-06-09 16:27:35 -0300

    update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-
+const objetosCena = [];
+let contadorCubos = 0;
 const cena = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -29,6 +30,59 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cubo = new THREE.Mesh(geometria, material);
 
 cena.add(cubo);
+
+function adicionarCubo() {
+
+    contadorCubos++;
+
+    const geometria = new THREE.BoxGeometry();
+
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+
+    const cubo = new THREE.Mesh(
+        geometria,
+        material
+    );
+
+    cubo.position.x = contadorCubos * 2;
+
+    cubo.name = `Cubo_${contadorCubos}`;
+
+    cena.add(cubo);
+
+    objetosCena.push(cubo);
+
+    atualizarLista();
+}
+
+function atualizarLista() {
+
+    const lista =
+        document.getElementById("listaObjetos");
+
+    lista.innerHTML = "";
+
+    objetosCena.forEach(objeto => {
+
+        const option =
+            document.createElement("option");
+
+        option.text = objeto.name;
+
+        lista.appendChild(option);
+
+    });
+}
+
+document
+.getElementById("btnCubo")
+.addEventListener(
+    "click",
+    adicionarCubo
+);
+
 
 camera.position.z = 5;
 
